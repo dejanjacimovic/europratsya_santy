20e07d7cdf09d903c9cfcd40f7ef654f25edaf78
Dejan Jacimovic <dejan@stuntcoders.com>
2022-03-01 20:47:30 +0100

    Add job category document

---
+++ schemas/jobCategory.js
@@ -0,0 +1,26 @@
+export default {
+  name: 'job_category',
+  title: 'Job Category',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    },
+  ],
+};
