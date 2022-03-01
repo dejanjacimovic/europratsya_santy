100aff1eacc508989956783a8cd73c49f5034473
Dejan Jacimovic <dejan@stuntcoders.com>
2022-03-01 20:44:34 +0100

    Add company document

---
+++ schemas/company.js
@@ -0,0 +1,54 @@
+export default {
+  name: 'company',
+  title: 'Company',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'origin',
+      title: 'Origin',
+      type: 'reference',
+      to: { type: 'country' },
+    },
+    {
+      name: 'about',
+      title: 'About',
+      type: 'array',
+      of: [
+        {
+          title: 'Block',
+          type: 'block',
+          styles: [{ title: 'Normal', value: 'normal' }],
+          lists: [],
+        },
+      ],
+    },
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+    },
+  },
+};
