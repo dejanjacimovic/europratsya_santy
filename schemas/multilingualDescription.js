33a2c8f471482d4534491bc30659de94b9ffefa9
Dejan Jacimovic <dejan@stuntcoders.com>
2022-03-04 23:27:24 +0100

    Lift anonymous object types

---
+++ schemas/multilingualDescription.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'multilingualDescription',
+  title: 'Multilingual description',
+  type: 'object',
+  fields: [
+    {
+      title: 'English Description',
+      name: 'en',
+      type: 'blockContent',
+    },
+    {
+      title: 'Ukrainian Description',
+      name: 'uk',
+      type: 'blockContent',
+    },
+  ],
+};
---
+++ schemas/multilingualTitle.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'multilingualTitle',
+  title: 'Multilingual Title',
+  type: 'object',
+  fields: [
+    {
+      title: 'English Title',
+      name: 'en',
+      type: 'string',
+    },
+    {
+      title: 'Ukrainian Title',
+      name: 'uk',
+      type: 'string',
+    },
+  ],
+};
