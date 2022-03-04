94ebe0516c6224bbb70c01da1f2931dbcb49cd35
Dejan Jacimovic <dejan@stuntcoders.com>
2022-03-04 15:51:23 +0100

    Add abberivation, ISO and slug to the country

--- schemas/country.js
+++ schemas/country.js
@@ -9,6 +9,25 @@ export default {
       type: 'string',
     },
     {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'abbreviation',
+      title: 'Abbreviation',
+      type: 'string',
+    },
+    {
+      name: 'iso',
+      title: 'ISO Code',
+      type: 'string',
+    },
+    {
       name: 'description',
       title: 'Description',
       type: 'text',
